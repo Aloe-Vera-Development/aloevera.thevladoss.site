9cdbf769ab479affbe56b81409f6a5c605b384a9
Vladislav Osin <osinvladislav@yandex.ru>
2022-04-24 03:30:29 +0300

    Обновлен навбар

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once 'services/UserService.php';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Вход</title>
 
-var_dump($_COOKIE);
+    <!-- Фавиконка -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../res/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../res/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../res/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../res/favicon/site.webmanifest">
 
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../styles/bootstrap.min.css">
+    <link rel="stylesheet" href="../js/bootstrap.bundle.js">
+
+
+    <!-- CSS конкретно для этой страницы -->
+    <link rel="stylesheet" href="../styles/custom/login.css">
+    <link rel="stylesheet" href="../styles/custom/styles.css">
+</head>
+
+<body>
+    <main>
+
+        <?php
+        require_once '/home/users/o/osinvladislav/domains/aloevera.thevladoss.site/navbar.php';
+
+        ?>
+    </main>
+
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,3 +1,6 @@
+<?php
+if (!isset($_COOKIE['login'])) {
+?>
 <div class="navbar_wrapper">
     <nav class="navbar_seedcare navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -8,19 +11,6 @@
         <div class="navbar-brand" href="#">
             <b><a style="color: #1A5B20">Seed</a><a style="color: #45D08D">Care</a></b>
         </div>
-        <!-- <div class="navbar_collapse_disabled" id="navbarTogglerDemo"> -->
-        <!--            <div class="collapse navbar-collapse" id="navbarTogglerDemo">-->
-        <!--                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">-->
-        <!--                    <li class="nav-item" >-->
-        <!--                        <a href="#" style="color: #0D0D0D66">Главная</a>-->
-        <!--                    </li>-->
-        <!--                    <li class="nav-item">-->
-        <!--                        <a href="#" style="color: #0D0D0D66">Календарь</a>-->
-        <!--                    </li>-->
-        <!--                    <li class="nav-item">-->
-        <!--                        <a href="#" style="color: #0D0D0D66">Настройки</a>-->
-        <!--                    </li>-->
-        <!--                </ul>-->
         <div class="flex_spacer"></div>
         <div>
             <button class="btn text-light rounded-pill" style="background: #45D08D91; width: 162px; height: 38px;" onclick="logBtn()">Войти</button>
@@ -28,3 +18,46 @@
         </div>
     </nav>
 </div>
+<?php
+} else {
+?>
+<div class="navbar_wrapper">
+    <nav class="navbar_seedcare navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-brand" href="#">
+            <b><a style="color: #0D0D0D66">SeedCare</a></b>
+        </div>
+        <div class="flex_spacer"></div>
+
+        <div class="navbar_collapse_disabled" id="navbarTogglerDemo">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 w-100">
+                    <li class="nav-item" >
+                        <a href="https://aloevera.thevladoss.site/" style="color: #0D0D0D66">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://aloevera.thevladoss.site/" style="color: #0D0D0D66">Календарь</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://aloevera.thevladoss.site/" style="color: #0D0D0D66">Настройки</a>
+                    </li>
+                </ul>
+
+                <div class="flex_spacer"></div>
+
+                <div class="d-flex justify-content-end" style="align-items: center">
+                    <!--          Иконка колокольчика          -->
+                    <img src="../res/bell.svg" alt="" style="width:20px;height:20px;margin-right: 16px;">
+                    <!--           Аватарка человечка         -->
+                    <img src="<?=$_COOKIE['photo']?>" alt="" style="width:30px;height:30px;border-radius: 50%;object-fit: cover;">
+                </div>
+            </div>
+    </nav>
+</div>
+<?php
+}
+?>
